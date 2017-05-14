475662afe86441b09dd574d9ecaf1599f9347a0a
Matt Brahe <mattbrahe@gmail.com>
2017-05-14 17:41:44 -0400

    Black and white tree

--- Assets/Scripts/PerspectiveScript.cs
+++ Assets/Scripts/PerspectiveScript.cs
@@ -28,26 +28,25 @@ public class PerspectiveScript : MonoBehaviour {
 		tempPosition.y = Camera.main.ViewportToWorldPoint (new Vector2(0, Mathf.Sin (Mathf.PI / 2 * (y)))).y;
 		tempPosition.z = Camera.main.WorldToViewportPoint (tempPosition).y;
 		tempPosition.x += cs.GetChange ().x * y;
-		Debug.Log (y);
-		Debug.Log (cs.GetChange ().x * y);
+
 		transform.position = tempPosition;
 
 		if (y < 0) {
 			y = 0;
 		}
 
-		transform.localScale = new Vector3 (Mathf.Sin (Mathf.PI / 2 * (1 - y/.5f)) * 6, Mathf.Sin (Mathf.PI / 2 * (1 - y/.5f)) * 6, 1);
+		transform.localScale = new Vector3 (Mathf.Sin (Mathf.PI / 2 * (1 - y/.5f)) * 10, Mathf.Sin (Mathf.PI / 2 * (1 - y/.5f)) * 10, 1);
 		
 		if (y > .3f) {
 			sr.enabled = false;
 		} else if (y > .2f) {
 			sr.enabled = true;
 
-			Color tempColor = sr.color;
+			/*Color tempColor = sr.color;
 			tempColor.r = (.25f - y) * 10;
 			tempColor.g = (.25f - y) * 10;
 			tempColor.b = (.25f - y) * 10;
-			sr.color = tempColor;
+			sr.color = tempColor;*/
 		} else {
 			sr.enabled = true;
 		}
